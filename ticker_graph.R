e3e18119cb5637aac3331c274329f85168b244da
msmith91 <mikesmith1991@gmail.com>
2016-01-24 18:40:44 -0600

    Create ticker_graph.R

---
+++ ticker_graph.R
@@ -0,0 +1,20 @@
+#pulls from csv table of stock prices
+#to use, run Rscript ticker_graph.R "NLSN" (for nlsn graph)
+#outputs graph to location designated
+
+#get command line arguments
+args <- commandArgs(TRUE)
+
+#function for pulling mysql output and putting into graph on server
+price_graph <- function(ticker){
+stocks <- read.csv(file="/home/mike/mysql_output/stocks_py_date.csv")
+stock <- stocks[grep(ticker,stocks$Stock),]
+for_graph <- data.frame(Stock=stock$Stock, Price=stock$Price)
+cat('"','/var/www/projects/stocks/images/',ticker,'.jpeg','"',sep="");
+jpeg(paste('/var/www/projects/stocks/images/',ticker,'.jpeg',sep=''))
+plot(row.names(for_graph),for_graph$Price,main=ticker,type="l")
+dev.off()
+}
+
+#run command
+price_graph(args[1])
