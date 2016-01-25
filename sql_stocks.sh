d2f32edbf870d92c6e1afbce8a851f0eea3a40da
msmith91 <mikesmith1991@gmail.com>
2016-01-24 18:49:49 -0600

    Update sql_stocks.sh

--- sql_stocks.sh
+++ sql_stocks.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+#master script - pulls the data from google with sql_stocks.py for GOOG (Google) and places it into a MYSQL database
+#              - converts that sql database into a csv with output_mysql.sh
+#              - graphs the data with ticker_graph.R
+#              - outputs log with date prefix
+#I added this to my crontab to run this script every 10 minutes during trading hours. (see results at mikeandpj.com/projects/stocks)
+
 exec > $(date +%m%d%Y_%H:%M).log
 
-/usr/bin/python sql_stocks.py NLSN
-
 /usr/bin/python sql_stocks.py GOOG
+
+/usr/bin/sh output_mysql.sh
+
+/usr/bin/Rscript ticker_graph.R GOOG
+
